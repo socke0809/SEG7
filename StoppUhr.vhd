767a12fd3bbecfe6e66e19ff5b70935e1b789938
socke0809 <steffi@dangl-muenchen.de>
2014-04-03 00:26:20 +0200

    added START and STOP BTN

--- StoppUhr.vhd
+++ StoppUhr.vhd
@@ -39,8 +39,9 @@ entity StoppUhr is
     Port (  SEG: out std_logic_vector(6 downto 0);
 				AN: out std_logic_vector(3 downto 0);
 				BTN_RST: in std_logic;
-				BTN_STOP: in std_logic;
+				BTN_STP: in std_logic;
 				BTN_START: in std_logic;
+				DP : out std_logic;
 				CLK : in  std_logic);
 end StoppUhr;
 
@@ -49,17 +50,30 @@ architecture Behavioral of StoppUhr is
 
 	signal SEK: integer range 0 to 60 := 0;
 	signal MIN: integer range 0 to 99 := 0;
+	signal SE: integer range 0 to 50000000 := 0;
+	
 	signal A_BUF: integer range 0 to 9999 := 0;
 	signal E_BUF: integer range 0 to 9 := 0;
-	signal A_OLD: integer range 0 to 9999 := 0;
+	signal B: integer range 0 to 10;
+	
+	signal RES: integer range 0 to 1000; --RESULT
+	signal RE: integer range 0 to 9999; --REST
+	
+	
 	signal PRE: integer range 0 to 12500000:= 0;
 	signal COUNT: integer range 0 to 3;
+	
 	signal BTN_RST_OLD: std_logic;
+	signal BTN_STP_OLD: std_logic;
+	signal BTN_START_OLD: std_logic;
+	
+	signal temp: std_logic := '0';
+	
 	signal CE: std_logic := '0';
-	signal B: integer range 0 to 1000;
-	signal RES: integer range 0 to 1000;
-	signal RE: integer range 0 to 9999;
-	signal SE: integer range 0 to 50000000 := 0;
+	
+
+
+	
 	
 	
 	component Int_to_7SEG
@@ -118,76 +132,105 @@ begin
 process(CLK)
 	begin 
 		if rising_edge(CLK) then
-		
-			if BTN_RST = '1' and BTN_RST_OLD = '0' then 
+			if BTN_START = '1' and BTN_START_OLD = '0' then
+				temp <= '1';
+			end if;
+			
+			if BTN_RST = '1' and BTN_RST_OLD = '0' then
+					temp <= '0';
 					SEK <= 0;
 					MIN <= 0;
+					SE <= 0;
 			end if;
-			--if (BTN_STOP = '0' ) or (BTN_STR = '1' and BTN_STR_OLD = '0') then
 			
-				if SE < 50000000   then  
-					SE <= SE +1;
-			
+			if  temp = '1' then
+				if BTN_STP = '1' and BTN_STP_OLD = '0' then
+					temp <= '0';
 				else
 					if SEK >= 59 then 
-						SEK <= 0;
-						MIN <= MIN +1;
-					else
-						SEK <= SEK +1;
+							SEK <= 0;
+							MIN <= MIN +1;
+						else
+							if SE < 50000000   then  
+								SE <= SE +1;
+							else
+								SEK <= SEK +1;
+								SE <= 0;
+							end if;
 					end if;
-			--else if B 
+					
 				end if;
+			end if;
 			
-			BTN_RST_OLD <= BTN_RST;
+		BTN_RST_OLD <= BTN_RST;
+		BTN_STP_OLD <= BTN_STP;
+		BTN_START_OLD <= BTN_START;
 		
 		end if;
-	end process; 
+
+end process; 
+				
+--		
+--			if BTN_RST = '1'  then --and BTN_RST_OLD = '0'
+--					SEK <= 0;
+--					MIN <= 0;
+--			
+			--
+--			else 
+--				if (BTN_STOP = '0' ) or (BTN_STR = '1' and BTN_STR_OLD = '0') then
+--					if SE < 50000000   then  
+--						SE <= SE +1;
+--			
+--					else
+--						if SEK >= 59 then 
+--							SEK <= 0;
+--							MIN <= MIN +1;
+--						else
+--							SEK <= SEK +1;
+--						end if;
+--						SE <= 0;
+--					end if;
+--				end if;
+--		end if;
+--			
+
 
 
 
 process (CLK)
 begin
 	if rising_edge(CLK) AND CE='1' then
-		--if A /= A_OLD then
-			--A_BUF <= A;
-		--end if;
+		
 		case COUNT is
 			when 0 => 
 				AN <= NOT "0001";
 
 				E_BUF <= RES;
-				B <= 10; --100
+				B <= 10; 
 				A_BUF <= MIN;
 				
 				
-				--A_BUF <= A_BUF-((A_BUF/100)*100);
 	
 			when 1 =>
 				AN <= NOT "1000";
 				E_BUF <= RES;
 				B <= 1;
-				
 				A_BUF <= RE;
 				
-				--A_BUF <= A_BUF-((A_BUF/10)*10);
-	
 			when 2 =>
 				AN <= NOT "0100";
 				E_BUF <= RES;
+				DP <= '0';
 				B <= 10;
-				--a_buf
 				A_BUF <=SEK;
-			
-				
 
 	
 			when 3 =>
 				AN <= NOT "0010";
 				E_BUF <= RES;
 				B <= 1; --1000
+				DP <= '1';
 				A_BUF <=RE;
-			
-				--A_BUF <= A_BUF-((A_BUF/1000)*1000);
 	
 			when others =>
 	
